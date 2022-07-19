908adb440123c90c5860071c333761d2f28aebd7
Carlos Pontón <carlitosponton@hotmail.com>
2022-07-19 23:11:00 +0200

    Refactor course migration

--- migrations/20220719200825_create_courses_table.ts
+++ migrations/20220719200825_create_courses_table.ts
@@ -4,9 +4,8 @@ export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('courses', (table) => {
         table.string('id', 36).primary()
         table.string('name', 36).notNullable()
+        table.string('slug', 256).notNullable()
         table.string('description', 512).notNullable()
-        table.string('about_me', 2048).notNullable()
-        table.string('banner_hex_color', 7).notNullable()
         table.string('image_url').nullable()
         table.string('repository', 256).nullable()
     })
