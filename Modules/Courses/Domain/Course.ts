d4f7b2f10c70865080d76b9436486000c203cea7
Carlos Pontón <carlitosponton@hotmail.com>
2022-07-23 15:06:16 +0200

    Run yarn lint

--- Modules/Courses/Domain/Course.ts
+++ Modules/Courses/Domain/Course.ts
@@ -1,27 +1,27 @@
 export class Course {
-    private readonly id: string
-    private readonly name: string
-    private readonly slug: string
-    private readonly description: string
-    private readonly imageUrl: string
-    private readonly publishedAt: Date
-    private readonly lastUpdate: Date
+  private readonly id: string
+  private readonly name: string
+  private readonly slug: string
+  private readonly description: string
+  private readonly imageUrl: string
+  private readonly publishedAt: Date
+  private readonly lastUpdate: Date
 
-    constructor(
-        id: string,
-        name: string,
-        slug: string,
-        description: string,
-        imageUrl: string,
-        publishedAt: Date,
-        lastUpdate: Date
-    ) {
-        this.id = id
-        this.name = name
-        this.slug = slug
-        this.description = description
-        this.imageUrl = imageUrl
-        this.publishedAt = publishedAt
-        this.lastUpdate = lastUpdate
-    }
+  constructor (
+    id: string,
+    name: string,
+    slug: string,
+    description: string,
+    imageUrl: string,
+    publishedAt: Date,
+    lastUpdate: Date
+  ) {
+    this.id = id
+    this.name = name
+    this.slug = slug
+    this.description = description
+    this.imageUrl = imageUrl
+    this.publishedAt = publishedAt
+    this.lastUpdate = lastUpdate
+  }
 }
--- Modules/Posts/Domain/Post.ts
+++ Modules/Posts/Domain/Post.ts
@@ -1,37 +1,36 @@
 export class Post {
-    private readonly id: string
-    private readonly slug: string
-    private readonly parentId: string
-    private readonly title: string
-    private readonly imageUrl: string
-    private readonly readTime: number
-    private readonly publishedAt: Date
-    private readonly lastUpdate: Date
-    private readonly courseId: string | null
-    private readonly authorId: string
+  private readonly id: string
+  private readonly slug: string
+  private readonly parentId: string
+  private readonly title: string
+  private readonly imageUrl: string
+  private readonly readTime: number
+  private readonly publishedAt: Date
+  private readonly lastUpdate: Date
+  private readonly courseId: string | null
+  private readonly authorId: string
 
-    constructor(
-        id: string,
-        slug: string,
-        parentId: string,
-        title: string,
-        imageUrl: string,
-        readTime: number,
-        publishedAt: Date,
-        lastUpdate: Date,
-        courseId: string | null,
-        authorId: string
-    ) {
-        this.id = id
-        this.slug = slug
-        this.parentId = parentId
-        this.title = title
-        this.imageUrl = imageUrl
-        this.readTime = readTime
-        this.publishedAt = publishedAt
-        this.lastUpdate = lastUpdate
-        this.courseId = courseId
-        this.authorId = authorId
-    }
-
+  constructor (
+    id: string,
+    slug: string,
+    parentId: string,
+    title: string,
+    imageUrl: string,
+    readTime: number,
+    publishedAt: Date,
+    lastUpdate: Date,
+    courseId: string | null,
+    authorId: string
+  ) {
+    this.id = id
+    this.slug = slug
+    this.parentId = parentId
+    this.title = title
+    this.imageUrl = imageUrl
+    this.readTime = readTime
+    this.publishedAt = publishedAt
+    this.lastUpdate = lastUpdate
+    this.courseId = courseId
+    this.authorId = authorId
+  }
 }
